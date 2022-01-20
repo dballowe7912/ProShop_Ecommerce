7c1b676cc2f70105d9e4255af45d78385bd9daa3
Donald Ballowe <dballowe7912@gmail.com>
2022-01-20 17:44:27 -0600

    Added loader and message components, hooked up redux to product page, end 31

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import React, {  useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import axios from 'axios';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+
+import { listProductDetails } from '../actions/productActions';
 
 const ProductScreen = () => {
+  const dispatch = useDispatch();
+  const productDetails = useSelector(state => state.productDetails);
+  const { loading, error, product } = productDetails;
   const params = useParams();
-  const [ product, setProduct ] = useState({}); 
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${params.id}`);
-
-      setProduct(data);
-    }
-
-    fetchProduct();
-  }, []);
+    dispatch(listProductDetails(params.id))
+  }, [dispatch, params]);
 
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>Go Back</Link>
+      {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : (
       <Row>
         <Col md={6}>
           <Image src={product.image} alt={product.name} fluid/>
@@ -74,6 +74,7 @@ const ProductScreen = () => {
           </Card>
         </Col>
       </Row>
+      )}
     </>
   )
 };
